38568975e328f62b7f7457ca42d2af421e787ab2
javacook <javacook@gmx.de>
2018-03-15 08:29:49 +0100

    Comments

--- src/main/kotlin/KotlinSpeechletRequestHandler.kt
+++ src/main/kotlin/KotlinSpeechletRequestHandler.kt
@@ -6,19 +6,20 @@ import com.fasterxml.jackson.module.kotlin.*
 data class HandlerInput(val who: String)
 data class HandlerOutput(val message: String)
 
-class KotlinSpeechletRequestHandler : SpeechletRequestStreamHandler(KotlinSpeechlet(), supportedApplicationIds) {
+// DEPENDENCY: class "de.kotlincook.KotlinSpeechletRequestHandler" must be set in the AWS configuration
+class KotlinSpeechletRequestHandler : SpeechletRequestStreamHandler(KotlinBlogSpeechlet(), supportedApplicationIds) {
 
     companion object {
         private val supportedApplicationIds = HashSet<String>()
         init {
-            // Fill here the skill id of your generated Skill
+            // DEPENDENCY: Take this ID from the Skill Developer Console generated there
             supportedApplicationIds.add("amzn1.ask.skill.50b9f4ef-1c02-4e72-bd4c-1bbdf1df61f5")
         }
     }
 
     val mapper = jacksonObjectMapper()
 
-    fun handler(input: InputStream, output: OutputStream): Unit {
+    fun handler(input: InputStream, output: OutputStream) {
         val inputObj = mapper.readValue<HandlerInput>(input)
         mapper.writeValue(output, HandlerOutput("Hello ${inputObj.who}"))
     }
